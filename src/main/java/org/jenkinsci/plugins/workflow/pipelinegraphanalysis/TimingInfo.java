0e14220dc4bff4f30d7de0cbca65aa80ac6bc7c0
Sam Van Oort <samvanoort@gmail.com>
2016-07-26 11:00:45 -0400

    Remove unused bits in TimingInfo

--- src/main/java/org/jenkinsci/plugins/workflow/pipelinegraphanalysis/TimingInfo.java
+++ src/main/java/org/jenkinsci/plugins/workflow/pipelinegraphanalysis/TimingInfo.java
@@ -4,11 +4,6 @@ public class TimingInfo {
     private long totalDurationMillis;
     private long pauseDurationMillis;
 
-    public TimingInfo() {
-        this.totalDurationMillis = 0;
-        this.pauseDurationMillis = 0;
-    }
-
     public TimingInfo(long totalDurationMillis, long pauseDurationMillis) {
         this.totalDurationMillis = totalDurationMillis;
         this.pauseDurationMillis = pauseDurationMillis;
@@ -28,22 +23,5 @@ public class TimingInfo {
 
     public void setPauseDurationMillis(long pauseDurationMillis) {
         this.pauseDurationMillis = pauseDurationMillis;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || !(o instanceof TimingInfo)) {
-            return false;
-        } else if (this == o) {
-            return true;
-        }
-        TimingInfo ti = (TimingInfo) o;
-        return this.pauseDurationMillis == ti.pauseDurationMillis && this.totalDurationMillis == ti.totalDurationMillis;
-    }
-
-    @Override
-    public int hashCode() {
-        long mixed = (~pauseDurationMillis) ^ totalDurationMillis;  // Bitwise invert because both are often equal
-        return (int) (mixed ^ (mixed >>> 32));
     }
 }
