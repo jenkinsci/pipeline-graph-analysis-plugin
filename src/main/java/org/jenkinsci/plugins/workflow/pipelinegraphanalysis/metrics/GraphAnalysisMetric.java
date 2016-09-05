c0d32b75762bde0acd3b425105085c37de69c2ca
Sam Van Oort <samvanoort@gmail.com>
2016-09-05 11:48:27 -0400

    More work towards analysis metric

--- src/main/java/org/jenkinsci/plugins/workflow/pipelinegraphanalysis/metrics/GraphAnalysisMetric.java
+++ src/main/java/org/jenkinsci/plugins/workflow/pipelinegraphanalysis/metrics/GraphAnalysisMetric.java
@@ -2,12 +2,14 @@ package org.jenkinsci.plugins.workflow.pipelinegraphanalysis.metrics;
 
 import org.jenkinsci.plugins.workflow.graphanalysis.ChunkFinder;
 import org.jenkinsci.plugins.workflow.graphanalysis.FlowChunkWithContext;
+import org.jenkinsci.plugins.workflow.graphanalysis.SimpleChunkVisitor;
 
 import java.util.Collection;
 
 /**
  * Holds info about what analysis metrics we care about for pipeline graph analysis
- * TODO use describable/descriptor APIs here?
+ * TODO Use describable/descriptor APIs here?
+ * TODO Describable created with all the extraction information from descriptor?
  * @author Sam Van Oort
  */
 public abstract class GraphAnalysisMetric <MetricDataType> {
@@ -18,17 +20,30 @@ public abstract class GraphAnalysisMetric <MetricDataType> {
     /** How to combine serial values */
     public abstract MetricDataType combineSerial(Collection<MetricDataType> values);
 
-    /** What am I called? */
+    /** What am I called?
+     *  TODO attach to descriptor
+     */
     public abstract String getDisplayName();
 
-    /** Initialize a new metrics store for our use */
+    /** Initialize a new metrics store for our use
+     *  TODO attach to descriptor
+     */
     public MetricsStore<MetricDataType> createMetricsStore() {
         return new MetricsStore<MetricDataType>();
     }
 
-    /** Defines how this metric finds chunks */
+    /** Defines how this metric finds chunks
+     *  TODO attach to descriptor
+     */
     public abstract ChunkFinder createChunkFinder();
 
-    /** Try to find the metric value from chunk */
+    /** Creates a visitor class that operates upon chunk
+     *  TODO attach to descriptor
+     */
+    public abstract SimpleChunkVisitor createVisitor();
+
+    /** Try to find the metric value from chunk
+     *  TODO attach to descriptor
+     */
     public abstract MetricDataType extractValue(FlowChunkWithContext chunk);
 }
